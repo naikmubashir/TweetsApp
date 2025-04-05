9dbbf4e552fbc42d89133a0b5f8d1f8ef47e782e
Naik Mubashir <naikmubashir095@gmail.com>
2025-04-05 20:17:52 +0530

    Add Login and Register User routes

--- TweetsApp/tweet/forms.py
+++ TweetsApp/tweet/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 from .models import Tweet
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class TweetForm(forms.ModelForm):
     class Meta:
         model=Tweet
         fields=['text','photo'] #from Tweet model
 
-class Regi
+class UserRegistrationForm(UserCreationForm): #default form from django
+    email=forms.EmailField() #extra field to be added to the default User model
+    class Meta:
+        model=User
+        fields=('username', 'email', 'password1', 'password2')
--- TweetsApp/tweet/views.py
+++ TweetsApp/tweet/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404 ,redirect
 from .models import Tweet
-from .forms import TweetForm
+from .forms import TweetForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import  login
 # Create your views here.
 def index(request):
     return render(request,'index.html')
@@ -44,3 +45,16 @@ def tweet_delete(request, tweet_id):
         tweet.delete()
         return redirect('tweet_list')
     return render(request,'tweet_confirm_delete.html',{'tweet':tweet})
+
+def register(request):
+    if request.method=='POST':
+        form =UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user=form.save(commit=False)
+            user.set_password(form.cleaned_data['password1'])
+            user.save()
+            login(request, user)
+            return redirect('tweet_list')
+    else:
+        form=UserRegistrationForm()
+    return render(request, 'registration/register.html', {'form':form})
